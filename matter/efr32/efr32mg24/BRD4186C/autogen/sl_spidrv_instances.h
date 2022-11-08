b4012877079c1736b2b101ad1deb9e95cbcd8c1c
thashok <Thota.Ashok@silabs.com>
2022-11-08 10:08:39 +0530

    pin multiplexing

--- matter/efr32/efr32mg24/BRD4186C/autogen/sl_spidrv_instances.h
+++ matter/efr32/efr32mg24/BRD4186C/autogen/sl_spidrv_instances.h
@@ -6,6 +6,20 @@ extern "C" {
 #endif
 
 #include "spidrv.h"
+
+#define LCD_BIT_RATE     1100000
+#define EXP_HDR_BIT_RATE 16000000
+#define EFLASH           16000000
+
+enum BAUDRATE_SET {
+  BAUDRATE_SET_EXP_HDR = 0,
+  BAUDRATE_SET_LCD     = 1,
+  BAUDRATE_SET_EFLASH  = 2,
+};
+
+extern enum BAUDRATE_SET baudrate_set_t;
+void SPI_baudrate_set(uint32_t);
+int LCD_baudrate_set(void);
 #ifdef RS911X_WIFI
 extern SPIDRV_Handle_t sl_spidrv_eusart_exp_handle;
 #endif
